a4c56b8b24af550865ebf7e59f2b4162dc578b84
Andrew Teologov <teologov.and@gmail.com>
2017-01-29 20:26:58 +0200

    minor refactoring to ES6

--- index.js
+++ index.js
@@ -2,51 +2,53 @@
  * Path to regexp lib wrapper
  * @author Andrew Teologov <teologov.and@gmail.com>
  * @date 16.04.14
- * TODO AT: tests
  */
-var path = require( 'path-to-regexp' );
+
+"use strict";
+
+const path = require('path-to-regexp');
 
 /**
  * Routes lib
  * @type {exports}
  */
-exports = module.exports = function( options ) {
-    options = options || {};
-    
-    /**
-     * String decoder
-     * @param {String} str
-     * @returns {*}
-     */
-    function decodeUri( str ) {
-        try {
-            str = decodeURIComponent( str );
-        } catch( e ) {
-            throw new Error( 'Cannot decodeURIComponent: ' + str );
-        }
-        return str;
+module.exports = function(options) {
+  options = options || {};
+  
+  /**
+   * String decoder
+   * @param {String} str
+   * @returns {*}
+   */
+  function decodeUri(str) {
+    try {
+      str = decodeURIComponent(str);
+    } catch(e) {
+      throw new Error(`Cannot decodeURIComponent: ${str}` );
     }
+    return str;
+  }
+  
+  return function(route) {
+    const keys = [];
+    const reg = path.apply(this, [route, keys, options]);
     
-    return function( route ) {
-        var keys = [],
-        reg = path.apply( this, [ route, keys, options ]);
-        
-        return function( route, params ) {
-            var res = reg.exec( route ),
-            	params = params || {};
-            
-            if ( !res )
-                return false;
-            
-            for ( var i = 1, l = res.length; i < l; i++ ) {
-                
-                if ( res[ i ] === undefined )
-                    continue;
-                
-                params[ keys[ i - 1 ].name ] = decodeUri( res[ i ]);
-            }
-            
-            return params;
-        }
+    return function(route, params) {
+      const res = reg.exec(route);
+      const	params = params || {};
+      
+      if (!res) {
+        return false;
+      }
+      
+      for (let i = 1, l = res.length; i < l; i++) {
+        if (!res[i]) {
+          continue;
+        }  
+        params[keys[i - 1].name] = decodeUri(res[ i ]);
+      }
+      
+      return params;
     }
+  }
 };
