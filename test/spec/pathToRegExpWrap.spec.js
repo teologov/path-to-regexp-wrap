a1cb2f309b9f4cea4578cc3f445e8909928017ee
Andrew Teologov <teologov.and@gmail.com>
2017-01-29 20:38:00 +0200

    tests scaffolding

---
+++ test/spec/pathToRegExpWrap.spec.js
@@ -0,0 +1,16 @@
+/*!
+ * path-to-regexp-wrap spec
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ */
+
+/* global define, describe, beforeEach, afterEach, it, assert, sinon, teoBase  */
+
+"use strict";
+
+describe('Testing path-to-regexp-wrap', () => {
+
+	it('Should pass', () => {
+		assert.isTrue(true);
+	});
+
+});
